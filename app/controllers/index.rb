63cb9dbc778c52576bb28c71cd9b85429948a2de
elizaway <elizadunaway@gmail.com>
2015-11-08 20:10:24 -0500

    just messing around

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,11 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "[LOG] Responding to a GET request for /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
+end
+
+get '/sandbox' do
+  puts "[LOG] Responding to a POST request to /sandbox"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_sandbox
 end
