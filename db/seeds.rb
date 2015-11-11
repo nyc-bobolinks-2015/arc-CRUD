557aec090cbb5768850a25b0507098bc8f70708f
Arcangelo Petretta <arcpetretta@gmail.com>
2015-11-10 23:27:29 -0500

    its half past 11. Done for the day.

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+Knick.create([{:name => 'Arron Afflalo', :jersey_number => 4, :position => 'SG'},
+               {:name => 'Lou Amundson', :jersey_number =>17 , :position => 'PF'},
+               {:name => 'Carmelo Anthony', :jersey_number =>7 , :position => 'SF'},
+               {:name => 'Jose Calderon', :jersey_number => 3 , :position => 'PG'},
+               {:name => 'Cleanthony Early', :jersey_number => 11 , :position => 'SF'},
+               {:name => 'Langston Galloway', :jersey_number => 2 , :position => 'PG'},
+               {:name => 'Jerian Grant', :jersey_number => 13 , :position => 'PG'},
+               {:name => 'Robin Lopez', :jersey_number => 8, :position => 'C'},
+               {:name => 'Kyle O\'Quinn', :jersey_number =>9 , :position => 'PF'},
+               {:name => 'Kristaps Porzingis', :jersey_number => 6 , :position => 'PF'},
+               {:name => 'Derrick Williams', :jersey_number =>23 , :position => 'PF'},
+               {:name => 'Sasha Vujacic', :jersey_number =>18 , :position => 'SG'}])
+
